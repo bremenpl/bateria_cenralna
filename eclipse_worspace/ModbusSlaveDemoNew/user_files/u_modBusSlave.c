d39c701d06d741baa4515ce9b60da54b2b782c27
Łukasz Przeniosło <bremenpl@gmail.com>
2017-03-01 16:28:19 +0100

    update

---
+++ eclipse_worspace/ModbusSlaveDemoNew/user_files/u_modBusSlave.c
@@ -0,0 +1,236 @@
+/*
+ * u_modBusSlave.c
+ *
+ *  Created on: 01.03.2017
+ *      Author: Lukasz
+ */
+
+
+/* Includes ------------------------------------------------------------------*/
+
+#include "u_modBusSlave.h"
+
+/* Defines and macros --------------------------------------------------------*/
+
+/* Enums and structs ---------------------------------------------------------*/
+
+/* Private variables ---------------------------------------------------------*/
+
+static mbsUart_t* mbs_u[MBS_MAX_NO_OF_SLAVES];
+static uint32_t mbs_slavesNr;
+
+/* Public variables ----------------------------------------------------------*/
+
+/* Fuction prototypes --------------------------------------------------------*/
+
+HAL_StatusTypeDef mbs_SendErrorResponse(mbgUart_t* uart, mbgFrame_t* mf, mbgExCode_t exCode);
+mbsUart_t* mbs_GetModuleFromUartHandle(UART_HandleTypeDef* uart);
+mbsUart_t* mbs_GetModuleFromTimerHandle(TIM_HandleTypeDef* timer);
+
+/* Function declarations -----------------------------------------------------*/
+
+/*
+ * @brief	Initializes the MODBUS Slave controller.
+ * @param	mbsu: pointer to modbus slave uart handle
+ * @param	noOfModules: number of modules to initialize (minimum 1)
+ * @return	HAL_OK if initialization was succesfull.
+ */
+HAL_StatusTypeDef mbs_Init(mbsUart_t* mbsu, size_t noOfModules)
+{
+	// check if modules number is not zero
+	if (!noOfModules)
+		noOfModules = 1;
+
+	HAL_StatusTypeDef retVal = HAL_OK;
+	mbs_slavesNr = noOfModules;
+	mbsUart_t* m = 0;
+	osMessageQDef_t msgDef_temp;
+
+	// threads definitions
+	osThreadDef(mbgRxTask, mbg_taskRxDequeue, osPriorityAboveNormal, noOfModules, 128);
+
+	// do everything for each module
+	for (size_t i = 0; i < noOfModules; i++)
+	{
+		// Convenience
+		m = mbsu + i;
+
+		// check mbsu pointers
+		if (!m)
+			return HAL_ERROR;
+
+		// validate slave address
+		if ((m->slaveAddr < MBS_MIN_ADDR) || (m->slaveAddr > MBS_MAX_ADDR))
+			return HAL_ERROR;
+
+		// assign parameter variables
+		mbs_u[i] = m;
+
+		// init the generic uart module
+		retVal += mbg_UartInit(&m->mbg);
+
+		// define queue
+		msgDef_temp.item_sz = sizeof(uint32_t);
+		msgDef_temp.queue_sz = MBG_RECV_QUEUE_LEN; // can accumulate up to 16 bytes
+
+		// create the queue
+		m->mbg.rxQ.msgQId = osMessageCreate(&msgDef_temp, NULL);
+		if (!m->mbg.rxQ.msgQId)
+			retVal++;
+
+		// create thread for dequeuing
+		m->mbg.rxQ.sysId = osThreadCreate(osThread(mbgRxTask), &m->mbg);
+		if (!m->mbg.rxQ.sysId)
+			retVal++;
+
+		// defaulr rx state
+		m->mbg.rxState = e_mbgRxState_addr;
+	}
+
+	assert_param(!retVal);
+	return retVal;
+}
+
+/*
+ * @brief	If \ref uart is found in one of the slave structs, the slave struct
+ * 			in which it was found is returned. Otherwised zero is returned.
+ * @param	uart: handle
+ * @return	modbus slave uart module pointer or zero.
+ */
+mbsUart_t* mbs_GetModuleFromUartHandle(UART_HandleTypeDef* uart)
+{
+	if (!uart)
+		return 0;
+
+	for (size_t i = 0; i < mbs_slavesNr; i++)
+	{
+		if (uart == mbs_u[i]->mbg.handle)
+			return mbs_u[i];
+	}
+
+	return 0;
+}
+
+/*
+ * @brief	If \ref timer is found in one of the slave structs, the slave struct
+ * 			in which it was found is returned. Otherwised zero is returned.
+ * @param	timer: handle
+ * @return	modbus slave timer module pointer or zero.
+ */
+mbsUart_t* mbs_GetModuleFromTimerHandle(TIM_HandleTypeDef* timer)
+{
+	if (!timer)
+		return 0;
+
+	for (size_t i = 0; i < mbs_slavesNr; i++)
+	{
+		if (timer == mbs_u[i]->mbg.rxQ.toutTim)
+			return mbs_u[i];
+	}
+
+	return 0;
+}
+
+/*
+ * @brief	Send an exception code frame to the master in response to a request.
+ * @param	uart: uart handler struct pointer.
+ * @param	mf: pointer to a received modbus frame, that will now be used as data
+ * 			container for creating a response frame. This is done like this due to
+ * 			memory and speed optimalisation reasons.
+ * 			The protocol is half duplex so its safe.
+ * @param	exCode: Exception code of type \ref mbsExCode_t.
+ * @return
+ */
+HAL_StatusTypeDef mbs_SendErrorResponse(mbgUart_t* uart, mbgFrame_t* mf, mbgExCode_t exCode)
+{
+	if (!uart->handle || !mf)
+		return HAL_ERROR;
+
+	// modify function code (becomes error code)
+	mf->code |= MBS_EXCODE_ADDR_OR;
+
+	// save exception code in data field
+	mf->data[0] = (uint8_t)exCode;
+
+	// update data length
+	mf->dataLen = 1;
+
+	// send the data and return
+	return mbg_SendFrame(uart, mf);
+}
+
+/*
+ * @brief	ModBus SLAVE strong override function.
+ * 			Use it to parse REQUESTS from the master device.
+ * 			Return if \ref uHandle does not match configured periph.
+ * @param	uHandle: pointer to the uart modbus slave struct.
+ */
+void mbs_uartRxRoutine(UART_HandleTypeDef* uHandle)
+{
+	assert_param(uHandle);
+
+	// check if handle is known
+	mbsUart_t* mbs = mbs_GetModuleFromUartHandle(uHandle);
+
+	if (mbs)
+		mbg_ByteReceived(&mbs->mbg);
+}
+
+/*
+ * @brief	ModBus SLAVE rx timeout override.
+ * @param	tim: pointer to the modbus timeout timer strict
+ */
+void mbs_uartRxTimeoutRoutine(TIM_HandleTypeDef* tim)
+{
+	assert_param(tim);
+
+	// check if handle is known
+	mbsUart_t* mbs = mbs_GetModuleFromTimerHandle(tim);
+
+	if (mbs)
+		mbg_RxTimeout(&mbs->mbg);
+}
+
+// overrides
+
+/*
+ * @brief	The default read holding registers function.
+ * 			If not overriden, it always returns illegar data address ex code.
+ * @param	mf: pointer to a modbus frame struct.
+ * @return  exception code, 0 if function executed properly.
+ */
+__attribute__((weak)) mbgExCode_t mbs_CheckReadHoldingRegisters(const mbsUart_t* const mbs,
+		mbgFrame_t* mf)
+{
+	return e_mbsExCode_illegalDataAddr;
+}
+
+/*
+ * @brief	The default read coils function.
+ * 			If not overriden, it always returns illegar data address ex code.
+ * @param	mf: pointer to a modbus frame struct.
+ * @return  exception code, 0 if function executed properly.
+ */
+__attribute__((weak)) mbgExCode_t mbs_CheckReadCoils(mbgFrame_t* mf)
+{
+	return e_mbsExCode_illegalDataAddr;
+}
+
+/*
+ * @brief	The default 05 (0x05) Write Single Coil function.
+ * 			If not overriden, it always returns illegar data address ex code.
+ * @param	mf: pointer to a modbus frame struct.
+ * @return  exception code, 0 if function executed properly.
+ */
+__attribute__((weak)) mbgExCode_t mbs_CheckWriteSingleCoil(mbgFrame_t* mf)
+{
+	return e_mbsExCode_illegalDataAddr;
+}
+
+
+
+
+
+
+
+
---
+++ eclipse_worspace/ModbusSlaveDemoNew/user_files/u_modBusSlave.h
@@ -0,0 +1,46 @@
+/*
+ * u_modBusSlave.h
+ *
+ *  Created on: 01.03.2017
+ *      Author: Lukasz
+ */
+
+#ifndef U_MODBUSSLAVE_H_
+#define U_MODBUSSLAVE_H_
+
+/* Includes ------------------------------------------------------------------*/
+#include "u_modBusGeneric.h"
+
+/* Defines and macros --------------------------------------------------------*/
+#define MBS_MAX_NO_OF_SLAVES	4
+
+#define MBS_MIN_ADDR			1
+#define MBS_MAX_ADDR			247
+
+#define MBS_EXCODE_ADDR_OR		0x80
+
+/* Enums and structs ---------------------------------------------------------*/
+
+typedef struct
+{
+	mbgUart_t			mbg;			/*!< Generic modbus functions */
+	uint8_t				slaveAddr;		/*!< Modbus slave address */
+
+} mbsUart_t;
+
+/* Private variables ---------------------------------------------------------*/
+
+/* Public variables ----------------------------------------------------------*/
+
+/* Fuction prototypes --------------------------------------------------------*/
+HAL_StatusTypeDef mbs_Init(mbsUart_t* mbsu, size_t noOfModules);
+
+// override functions
+mbgExCode_t mbs_CheckReadHoldingRegisters(const mbsUart_t* const mbs, mbgFrame_t* mf);
+mbgExCode_t mbs_CheckReadCoils(mbgFrame_t* mf);
+mbgExCode_t mbs_CheckWriteSingleCoil(mbgFrame_t* mf);
+
+/* Function declarations -----------------------------------------------------*/
+
+
+#endif /* U_MODBUSSLAVE_H_ */
